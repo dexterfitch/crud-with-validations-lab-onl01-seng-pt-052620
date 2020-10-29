bccd5051463b84c4daa2b0cb3be44da92140a7c1
Dexter Fitch <fitch.dexter@gmail.com>
2020-10-29 01:18:15 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,5 @@
 class Song < ApplicationRecord
   validates :title, presence: true
-  #   - Cannot be repeated by the same artist in the same year
   validates :released, inclusion: { in: [true, false] }
   validates :release_year, presence: true, if: :released
   validates :release_year, numericality: { less_than_or_equal_to: Time.now.year }, allow_nil: true

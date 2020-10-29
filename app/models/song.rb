de7c09f0bc8a9f063451a28e77457e55b5a52543
Dexter Fitch <fitch.dexter@gmail.com>
2020-10-29 01:15:28 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,10 @@
 class Song < ApplicationRecord
   validates :title, presence: true
   #   - Cannot be repeated by the same artist in the same year
-  validates :released, inclusion: { in: [true, false] }
-  validates :release_year, presence: true, if: :released
+  validates_inclusion_of :released, in: [true, false]
+  validates_presence_of :release_year, if: :released
   validates :release_year, numericality: { less_than_or_equal_to: Time.now.year }
+  #   - Must be less than or equal to the current year
   validates :artist_name, presence: true
 
   def unique_song?

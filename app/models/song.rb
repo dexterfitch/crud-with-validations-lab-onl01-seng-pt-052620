81ff13cdb49399eb0ac465314715e2ade58c3994
Dexter Fitch <fitch.dexter@gmail.com>
2020-10-29 01:18:06 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,7 +5,7 @@ class Song < ApplicationRecord
   validates :release_year, presence: true, if: :released
   validates :release_year, numericality: { less_than_or_equal_to: Time.now.year }, allow_nil: true
   validates :artist_name, presence: true
-
+  validate :unique_song
   def unique_song?
     @songs = Song.all
     if title
